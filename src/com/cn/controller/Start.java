03fb82f4e063eafffa3e5e57b49ab8863c951de5
kangwenbo <kwb789@163.com>
2017-06-14 17:50:45 +0800

    test3

--- src/com/cn/controller/Start.java
+++ src/com/cn/controller/Start.java
@@ -5,7 +5,11 @@ package com.cn.controller;
  */
 public class Start {
     public static void main(String[] args) {
+
         System.out.println("Start.main");
+        System.out.println("Start.main");
+        System.out.println("Start.main");
+
 
     }
 }
