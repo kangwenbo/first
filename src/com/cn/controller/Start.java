e49c6288f131451d2106116b30c18a8972d07f57
kangwenbo <kwb789@163.com>
2017-06-14 17:46:57 +0800

    Merge branch 'master' of https://github.com/kangwenbo/first

--- src/com/cn/controller/Start.java
+++ src/com/cn/controller/Start.java
@@ -4,8 +4,8 @@ package com.cn.controller;
  * Created by daojia on 2017-6-14.
  */
 public class Start {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        System.out.println("Start.main");
 
     }
 }
